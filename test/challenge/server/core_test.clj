0ecf71785f573da342f294c18b1b6fe8b652dd2c
pbaille <pierrebaille@gmail.com>
2018-11-12 10:09:29 +0100

    generative tests superstring case and refactor

--- test/challenge/server/core_test.clj
+++ test/challenge/server/core_test.clj
@@ -36,12 +36,24 @@
 
     (def str* (partial apply str))
 
+    (defn rand-int-between [min max]
+      (rand-nth (range min (inc max))))
+
+    (defn shuffle-str [s]
+      (str* (shuffle (into [] s))))
+
+    (defn rand-alpha-str [min max]
+      (str* (take (rand-int-between min max)
+                  (repeatedly #(rand-nth (seq alphas))))))
+
     (defn rand-substring [s]
-      (let [c (count s)]
-        (str* (take (rand-int c) s))))
+      (str* (take (rand-int (count s)) s)))
 
     (defn rand-shuffled-substring [s]
-      (str* (shuffle (into [] (rand-substring s)))))
+      (shuffle-str (rand-substring s)))
+
+    (defn rand-shuffled-superstring [s]
+      (shuffle-str (str s (rand-alpha-str 1 20))))
 
     (defn unused-alphas [s]
       (seq (apply disj alphas (set s))))
@@ -49,25 +61,33 @@
     (defn rand-non-substring [s]
       (when-let [others (unused-alphas s)]
         (let [s2 (take (inc (rand-int (dec (count others)))) others)]
-          (str* (shuffle (concat s s2)))))))
+          (shuffle-str (str* (rand-substring s) s2))))))
 
 (def alpha-str
   (gen/fmap str*
             (gen/vector (gen/elements alphas))))
 
+;; succeed on substrings
 (def scramble-1
   (prop/for-all [v alpha-str]
                 (scramble? v (rand-substring v))))
 
+;; succeed on shuffled substrings
 (def scramble-2
   (prop/for-all [v alpha-str]
                 (scramble? v (rand-shuffled-substring v))))
 
+;; fail on non-substrings
 (def scramble-3
   (prop/for-all [v alpha-str]
                 (if-let [v2 (rand-non-substring v)]
                   (not (scramble? v v2))
                   true)))
+
+;; fail on superstrings
+(def scramble-4
+  (prop/for-all [v alpha-str]
+                (not (scramble? v (rand-shuffled-superstring v)))))
 
 (deftest scramble-gen-tests
   (ct/do-template
@@ -75,4 +95,5 @@
     (is (:pass? (tc/quick-check 10000 x)))
     scramble-1
     scramble-2
-    scramble-3))
+    scramble-3
+    scramble-4))
